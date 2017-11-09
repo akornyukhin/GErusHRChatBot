a41767f1d9222ad26c04cccd777affc98459f82f
Kornyukhin <212632683@logon.ds.ge.com>
2017-11-09 14:58:13 +0300

    User and user's messages track applied'

--- echo_bot.py
+++ echo_bot.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+
 engine = create_engine('postgres://baujmpgp:qiMRgtF0Dp_dUgQTATZqoS0xLgsqLEGE@horton.elephantsql.com:5432/baujmpgp')
  
 from sqlalchemy.orm import sessionmaker
@@ -14,10 +15,6 @@ session.configure(bind=engine)
 
 local_timezone = tzlocal.get_localzone()
 bot = telebot.TeleBot("477403081:AAGkP0dT1k4i4BjHZCjegkqrt-wcVOQ-Lso")
-update = bot.get_updates()
-
-user_dict = {}
-number_of_users = 0
 
 # ORM
 Base = declarative_base()
@@ -27,33 +24,57 @@ class Message(Base):
     id = Column(Integer, primary_key=True)
     text = Column(String)
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key = True, index=True)
+    number_of_msg = Column(Integer)
+
+class HRdata(Base):
+    __tablename__ = 'hrdata'
+    id = Column(Integer, primary_key = True)
+    node_id = Column(Integer)
+    body = Column(String)
+    ref_node_id = Column(Integer)
+
 Base.metadata.create_all(engine)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    global number_of_users
     bot.reply_to(message, "Hello, I am GE HR Chat bot. To see help type /help")
     bot.send_message(message.chat.id, str(message.from_user))
 
-    user_dict['User {}'.format(random.random())] = message.from_user.id
-    number_of_users += 1
+    user = User(id = message.from_user.id, number_of_msg = 0)
+    s = session() 
 
-    bot.send_message(428535905, str(user_dict))
+    try:
+        s.add(user)
+        s.query(User).all()
+        s.commit()
+    except:
+        s.rollback()
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
     bot.reply_to(message, "I can answer most common HR questions. You ask wich one - my developer didn't tell me it yet :)")
 
+@bot.message_handler(commands=['ask'])
+def send_ask(message):
+    bot.send_message(message.chat.id, 'Chose what you want to know:')
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-
     bot.reply_to(message, str(datetime.fromtimestamp(float(message.date), local_timezone).strftime("%d-%m-%Y %H:%M:%S")) + "    " + message.text)
-    bot.send_message(428535905, message.text)
-
+    bot.send_message(428535905, message.text + "    "  + str(message.from_user.id))
+    
+    user_id = message.from_user.id
     message = Message(id=message.message_id, text=message.text)
+    
     s = session()
     s.add(message)
-    s.commit()
     s.query(Message).all()
+    s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
+    s.commit()
+
+
 
 bot.polling(none_stop=True)
