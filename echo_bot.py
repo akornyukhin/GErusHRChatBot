88dca527fb03817e30bf547b01287020b0b0257a
Kornyukhin <212632683@logon.ds.ge.com>
2017-11-17 19:17:26 +0300

    Buttons implemented, working through the whole tree, dead-ends should be implemented next time

--- echo_bot.py
+++ echo_bot.py
@@ -1,10 +1,12 @@
 import random
 from datetime import datetime
 import telebot
+from telebot import types
 import tzlocal
 from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy import create_engine
 
 engine = create_engine('postgres://baujmpgp:qiMRgtF0Dp_dUgQTATZqoS0xLgsqLEGE@horton.elephantsql.com:5432/baujmpgp')
@@ -39,8 +41,8 @@ class HRdata(Base):
 
 Base.metadata.create_all(engine)
 
-df = pd.read_sql('hrdata', engine, index_col = 'id')
-df['id'] = df.index.values
+df = pd.read_sql('hrdata', engine, index_col='id')
+list_of_values = df.loc[df.node_id == 1, ['id','body']].to_csv(sep="\t", header=False)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -61,36 +63,44 @@ def send_welcome(message):
 def send_help(message):
     bot.reply_to(message, "I can answer most common HR questions. You ask wich one - my developer didn't tell me it yet :)")
 
+def buttons(list):
+    markup = types.ReplyKeyboardMarkup(selective=True)
+    for item in list:
+        markup.row(types.KeyboardButton(item))
+
+    # 
+    # Generating a 'Ask another question' button needs to be implemented of dead-ends
+    # 
+    
+    return markup
+
 @bot.message_handler(commands=['ask'])
 def send_ask(message):
-    bot.send_message(message.chat.id, 'Chose what you want to know: \n' + df.loc[df['node_id'] == 1,'body'])
+    bot.send_message(message.chat.id, "Chose what you want to know: \n" + list_of_values,
+    reply_markup = buttons(df.loc[df.node_id == 1].index.astype('str')))
 
 @bot.message_handler(regexp = '\d')
 def send_answer(message):
     answer_id = int(message.text)
     ref_node_id = df.get_value(answer_id,'ref_node_id')
-    
-    # 
-    # Необходимо сделать обертку для id в str
-    # 
-    result = df.loc[df['node_id'] == ref_node_id, ['id','body']].values
-    result = "\n".join([" ".join(row) for row in result])
 
-    bot.reply_to(message, result)
+    result = df.loc[df['node_id'] == ref_node_id, ['body']].reset_index().to_csv(sep="\t", header=False, index=False)
+    markup = buttons(df.loc[df['node_id'] == ref_node_id].index.astype('str'))
+    bot.reply_to(message, result, reply_markup=markup)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, str(message.date) + "    " + message.text)
-    bot.send_message(428535905, message.text + "    "  + str(message.from_user.id))
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, str(message.date) + "    " + message.text)
+#     bot.send_message(428535905, message.text + "    "  + str(message.from_user.id))
     
-    user_id = message.from_user.id
-    message = Message(id=message.message_id, text=message.text)
+#     user_id = message.from_user.id
+#     message = Message(id=message.message_id, text=message.text)
     
-    s = session()
-    s.add(message)
-    s.query(Message).all()
-    s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
-    s.commit()
+#     s = session()
+#     s.add(message)
+#     s.query(Message).all()
+#     s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
+#     s.commit()
 
 
 
