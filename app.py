e8a89b44ef67b3ae9efb36d8f8a8657043443e8d
Kornyukhin <212632683@logon.ds.ge.com>
2017-11-23 13:54:52 +0300

    Trying to start

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+import logging
 import flask
 import pandas as pd
 import telebot
@@ -12,23 +12,34 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine(os.getenv('CSTRING'))
 session = sessionmaker()
 
-
-# Getting data from DB
-df = pd.read_sql('hrdata', engine, index_col='id')
-list_of_values = df.loc[df.node_id == 1, ['id','body']].to_csv(sep="\t", header=False)
+# # Getting data from DB
+# df = pd.read_sql('hrdata', engine, index_col='id')
+# list_of_values = df.loc[df.node_id == 1, ['id','body']].to_csv(sep="\t", header=False)
 
 # Bot init
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 
 # Webhook
 API_TOKEN = os.getenv('TOKEN')
-WEBHOOK_PORT = os.getenv("PORT")
-WEBHOOK_LISTEN = '0.0.0.0'
-WEBHOOK_HOST = json.loads(os.getenv('VCAP_APPLICATION'))['VCAP_APPLICATION']['application_uris']
 
+# if os.getenv("PORT"):
+#     WEBHOOK_PORT = int(os.getenv("PORT"))
+# else:
+#     WEBHOOK_PORT = 443
+
+WEBHOOK_PORT = 8443
+print(WEBHOOK_PORT)
+
+WEBHOOK_LISTEN = '0.0.0.0'
+
+if os.getenv('VCAP_APPLICATION'):
+    print(json.loads(os.getenv('VCAP_APPLICATION'))['application_uris'][0])
+    WEBHOOK_HOST = json.loads(os.getenv('VCAP_APPLICATION'))['application_uris'][0]
+else:
+    WEBHOOK_HOST = '165.225.72.91'
 
 WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/{}}/".format(API_TOKEN)
+WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
 
 # Flask init
 app = flask.Flask(__name__)
@@ -37,136 +48,136 @@ app = flask.Flask(__name__)
 def index():
     return ''
 
-# Process webhook calls
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
-def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
+# # Remove webhook, it fails sometimes the set if there is a previous webhook
+# bot.remove_webhook()
+# info = bot.get_webhook_info()
+# print(info)
 
-# ORM
-Base = declarative_base()
+# # Set webhook
+# bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH)
+# info = bot.get_webhook_info()
+# print(info)
 
-class Message(Base):
-    __tablename__ = 'messages'
-    id = Column(Integer, primary_key=True)
-    text = Column(String)
+# # Process webhook calls
+# @app.route(WEBHOOK_URL_PATH, methods=['POST'])
+# def webhook():
+#     print(flask.request)
+#     # if flask.request.headers.get('content-type') == 'application/json':
+    #     json_string = flask.request.get_data().decode('utf-8')
+    #     update = telebot.types.Update.de_json(json_string)
+    #     bot.process_new_updates([update])
+    #     return ''
+    # else:
+    #     flask.abort(403)
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key = True, index=True)
-    number_of_msg = Column(Integer)
+# # ORM
+# Base = declarative_base()
 
-class HRdata(Base):
-    __tablename__ = 'hrdata'
-    id = Column(Integer, primary_key = True)
-    node_id = Column(Integer)
-    body = Column(String)
-    ref_node_id = Column(Integer)
+# class Message(Base):
+#     __tablename__ = 'messages'
+#     id = Column(Integer, primary_key=True)
+#     text = Column(String)
 
-Base.metadata.create_all(engine)
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key = True, index=True)
+#     number_of_msg = Column(Integer)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello, I am GE HR Chat bot. To see help type /help. To ask the question type /ask.")
-    bot.send_message(message.chat.id, str(message.from_user))
+# class HRdata(Base):
+#     __tablename__ = 'hrdata'
+#     id = Column(Integer, primary_key = True)
+#     node_id = Column(Integer)
+#     body = Column(String)
+#     ref_node_id = Column(Integer)
 
-    user = User(id = message.from_user.id, number_of_msg = 0)
-    s = session()
+# Base.metadata.create_all(engine)
 
-    try:
-        s.add(user)
-        s.query(User).all()
-        s.commit()
-    except:
-        s.rollback()
+# @bot.message_handler(commands=['start'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Hello, I am GE HR Chat bot. To see help type /help. To ask the question type /ask.")
+#     bot.send_message(message.chat.id, str(message.from_user))
 
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.reply_to(message, "I can answer most common HR questions. Type /ask to begin.")
+#     user = User(id = message.from_user.id, number_of_msg = 0)
+#     s = session()
 
-    user_id = message.from_user.id
-    message = Message(id=message.message_id, text=message.text)
-    s = session()
-    s.add(message)
-    s.query(Message).all()
-    s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
-    s.commit()
+#     try:
+#         s.add(user)
+#         s.query(User).all()
+#         s.commit()
+#     except:
+#         s.rollback()
 
-@bot.message_handler(commands=['ask'])
-@bot.message_handler(regexp = '^Ask another question$')
-def send_ask(message):
-    bot.send_message(message.chat.id, "Chose what you want to know: \n" + list_of_values,
-    reply_markup = buttons(df.loc[df.node_id == 1].index.astype('str'))
-    )
+# @bot.message_handler(commands=['help'])
+# def send_help(message):
+#     bot.reply_to(message, "I can answer most common HR questions. Type /ask to begin.")
 
-    user_id = message.from_user.id
-    message = Message(id=message.message_id, text=message.text)
-    s = session()
-    s.add(message)
-    s.query(Message).all()
-    s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
-    s.commit()
+#     user_id = message.from_user.id
+#     message = Message(id=message.message_id, text=message.text)
+#     s = session()
+#     s.add(message)
+#     s.query(Message).all()
+#     s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
+#     s.commit()
 
-@bot.message_handler(regexp = '\d')
-def send_answer(message):
-    answer_id = int(message.text)
-    ref_node_id = df.get_value(answer_id,'ref_node_id')
+# @bot.message_handler(commands=['ask'])
+# @bot.message_handler(regexp = '^Ask another question$')
+# def send_ask(message):
+#     bot.send_message(message.chat.id, "Chose what you want to know: \n" + list_of_values,
+#     reply_markup = buttons(df.loc[df.node_id == 1].index.astype('str'))
+#     )
 
-    flag = False
-    if len(list(df.loc[df['node_id'] == ref_node_id, 'ref_node_id'])) == 1:
-        flag = True
+#     user_id = message.from_user.id
+#     message = Message(id=message.message_id, text=message.text)
+#     s = session()
+#     s.add(message)
+#     s.query(Message).all()
+#     s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
+#     s.commit()
 
-    result = df.loc[df['node_id'] == ref_node_id, ['body']].reset_index().to_csv(sep="\t", header=False, index=False)
-    markup = buttons(df.loc[df['node_id'] == ref_node_id].index.astype('str'), end=flag)
-    bot.reply_to(message, result, reply_markup=markup)
+# @bot.message_handler(regexp = '\d')
+# def send_answer(message):
+#     answer_id = int(message.text)
+#     ref_node_id = df.get_value(answer_id,'ref_node_id')
 
-    user_id = message.from_user.id
-    message = Message(id=message.message_id, text=message.text)
+#     flag = False
+#     if len(list(df.loc[df['node_id'] == ref_node_id, 'ref_node_id'])) == 1:
+#         flag = True
 
-    s = session()
-    s.add(message)
-    s.query(Message).all()
-    s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
-    s.commit()
+#     result = df.loc[df['node_id'] == ref_node_id, ['body']].reset_index().to_csv(sep="\t", header=False, index=False)
+#     markup = buttons(df.loc[df['node_id'] == ref_node_id].index.astype('str'), end=flag)
+#     bot.reply_to(message, result, reply_markup=markup)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    user_id = message.from_user.id
-    message = Message(id=message.message_id, text=message.text)
+#     user_id = message.from_user.id
+#     message = Message(id=message.message_id, text=message.text)
 
-    bot.reply_to(message, 'To ask a question type /ask')
+#     s = session()
+#     s.add(message)
+#     s.query(Message).all()
+#     s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
+#     s.commit()
 
-    s = session()
-    s.add(message)
-    s.query(Message).all()
-    s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
-    s.commit()
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     user_id = message.from_user.id
+#     message = Message(id=message.message_id, text=message.text)
 
-def buttons(list, end=False):
-    markup = types.ReplyKeyboardMarkup(selective=True)
-    if end == True:
-        markup.row(types.KeyboardButton('Ask another question'))
-    else:
-        for item in list:
-            markup.row(types.KeyboardButton(item))
-    return markup
+#     bot.reply_to(message, 'To ask a question type /ask')
 
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
+#     s = session()
+#     s.add(message)
+#     s.query(Message).all()
+#     s.query(User).filter(User.id == user_id).update({User.number_of_msg: User.number_of_msg + 1}, synchronize_session = False)
+#     s.commit()
 
-# Set webhook
-bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH)
+# def buttons(list, end=False):
+#     markup = types.ReplyKeyboardMarkup(selective=True)
+#     if end == True:
+#         markup.row(types.KeyboardButton('Ask another question'))
+#     else:
+#         for item in list:
+#             markup.row(types.KeyboardButton(item))
+#     return markup
 
-# Start flask server
-app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT, 
-        # port=os.getenv('PORT'),
-        debug=os.getenv('FLASK_DEBUG'))
-
-# if __name__=='__main__':
+if __name__=='__main__':
+    app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, debug=os.getenv('FLASK_DEBUG'))
 #     bot.polling(none_stop=True)
